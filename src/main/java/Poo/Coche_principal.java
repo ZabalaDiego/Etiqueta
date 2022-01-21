65c6685f8ed0066795ffc5b2fed60d2dd0651f12
zabaladiego <zabaladiego24@gmail.com>
2022-01-21 18:19:28 +0100

    Coche con metodos

--- src/main/java/Poo/Coche_principal.java
+++ src/main/java/Poo/Coche_principal.java
@@ -1,14 +1,37 @@
 
 package Poo;
 
+import java.util.Scanner;
+
 public class Coche_principal {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String color, asientos, acondicionador;
+        
         Coche Renault = new Coche();
-        Renault.setColor("verde");
+        
+        PedirDatos(sc, Renault);
+        
+        
         Renault.setAsientos("Si");
         Renault.setAcondicionador("no");
         System.out.println(Renault.getDatos());
+    }
+
+    private static void PedirDatos(Scanner sc, Coche Renault) {
+        String color;
+        String acondicionador;
+        String asientos;
+        System.out.println("Introduce el color del coche");
+        color = sc.nextLine();
+        Renault.setColor(color);
+        System.out.println("Contiene acondicionadores? si o no?");
+        acondicionador= sc.nextLine();
+        Renault.setAcondicionador(acondicionador);
+        System.out.println("Los asientos son de cuero? si o no?");
+        asientos = sc.nextLine();
+        Renault.setAsientos(asientos);
     }
     
 }
